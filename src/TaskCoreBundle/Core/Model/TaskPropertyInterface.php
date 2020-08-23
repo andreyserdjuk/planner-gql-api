78c779569aac4f57aab0935ec6af638c22380990
andreyserdjuk <ase6120@gmail.com>
2020-08-23 17:30:53 +0300

    added core interface doc

--- src/TaskCoreBundle/Core/Model/TaskPropertyInterface.php
+++ src/TaskCoreBundle/Core/Model/TaskPropertyInterface.php
@@ -2,6 +2,10 @@
 
 namespace Planner\TaskCoreBundle\Core\Model;
 
+/**
+ * Task can have multiple properties such as section: description, acceptance criteria, repository branch etc.
+ * As the logic of every property processing can differ, it can be provided by module supplies the property.
+ */
 interface TaskPropertyInterface
 {
     public function getId();
